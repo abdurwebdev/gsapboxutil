55fcb04931fec63fd4cd73b06beb18cf35c596e9
unknown <iabdurrehman12345@gmail.com>
2024-08-03 04:04:35 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,29 @@ import gsap from 'gsap';
 
 const App = () => {
   const [circles, setCircle] = useState(0);
-  const random = gsap.utils.random(-500, 500, 50);
+  const [moveY, setmoveY] = useState(0);
+  const [rotateX, setrotateX] = useState(0);
+  const randomX = gsap.utils.random(-500, 500, 50);
+  const randomY=gsap.utils.random(-300,300,50);
+  const rotates=gsap.utils.random(-360,360,0);
 
   useEffect(() => {
     gsap.to('.circle', {
       x: circles,
       duration: 1,
+      y:moveY,
+      rotate:rotateX
     });
-  }, [circles]);
+  },[circles,moveY,rotates] );
 
   return (
     <>
-    <div className="w-full h-screen flex-col gap-y-5 flex items-center justify-center text-white bg-[#111]">
+    <div className="w-full overflow-hidden boxes h-screen flex-col gap-y-5 flex items-center justify-center text-white bg-[#111]">
       <button
         onClick={() => {
-          setCircle(random);
-          console.log("hello");
+          setCircle(randomX);
+          setmoveY(randomY);
+          setrotateX(rotates);
           
         }}
         className="rounded-3xl text-2xl px-10 py-5 text-white bg-yellow-300"
