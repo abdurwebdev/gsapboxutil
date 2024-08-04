21a67eb8c1aff1437aa921b4b50a4564fb22be37
unknown <iabdurrehman12345@gmail.com>
2024-08-04 04:12:40 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react'
 import gsap from 'gsap';
-
+import { useGSAP } from '@gsap/react';
 const App = () => {
-  const [circles, setCircle] = useState(0);
-  const [moveY, setmoveY] = useState(0);
-  const [rotateX, setrotateX] = useState(0);
-  const randomX = gsap.utils.random(-500, 500, 50);
-  const randomY=gsap.utils.random(-300,300,50);
-  const rotates=gsap.utils.random(-360,360,0);
+  useGSAP(()=>{
+    const tl = gsap.timeline({ repeat: -1 });
 
-  useEffect(() => {
-    gsap.to('.circle', {
-      x: circles,
-      duration: 1,
-      y:moveY,
-      rotate:rotateX
+    
+    tl.to(".image-container", {
+      width: "100%",
+      duration: 2,
+      ease: "expo.inOut",
+      stagger: 2,
     });
-  },[circles,moveY,rotates] );
 
+    
+    tl.to("#text h1", {
+      top: 0,
+      ease: "expo.inOut",
+      stagger: 2,
+      duration: 1, 
+    },"-=6")
+    .to("#text h1", {
+      top: "-100%",
+      ease: "expo.inOut",
+      stagger: 2,
+      duration: 1, 
+      delay: 1, 
+    },"-=6");
+      
+  })
   return (
-    <>
-    <div className="w-full overflow-hidden boxes h-screen flex-col gap-y-5 flex items-center justify-center text-white bg-[#111]">
-      <button
-        onClick={() => {
-          setCircle(randomX);
-          setmoveY(randomY);
-          setrotateX(rotates);
-          
-        }}
-        className="rounded-3xl text-2xl px-10 py-5 text-white bg-yellow-300"
-      >
-        Animate
-      </button>
-      <div className="circle w-[200px] h-[200px] rounded-full bg-green-400"></div>
+    <div className='w-full h-screen bg-[#dadada] flex items-center justify-center'>
+      <div id="text" className='font-bold capitalize text-9xl font-[Kajiro]'>
+        <h1>CUBES.</h1>
+        <h1>DIAMONDS.</h1>
+        <h1>MIC.</h1>
+      </div>
+    <div className='w-[700px] h-[400px]  relative'>
+      <div id='one' className="image-container w-full h-full bg-yellow-200 absolute ">
+        <img  className='h-full object-cover' src="https://images.unsplash.com/photo-1655499603560-6f834760f8d5?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+      </div>
+      <div className="image-container w-0 bg-yellow-700 h-full  absolute">
+        <img className='h-full object-cover' src="https://images.unsplash.com/photo-1653405507161-da7d205d86f4?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+      </div>
+      <div  className="image-container w-0 h-full  absolute">
+        <img className='h-full object-cover' src="https://images.unsplash.com/photo-1645102322652-e5d7ed3fef8b?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+      </div>
     </div>
-    </>
-  );
-};
+  </div>
+  )
+}
 
-export default App;
+export default App
